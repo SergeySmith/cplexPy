92b29ed41e43ac5eb7d2680ab1422c19a626c368
Sergey Kuznetsov <sek101@pitt.edu>
2017-12-31 13:23:17 -0500

    added L-Shaped class

--- lshaped/lshaped.py
+++ lshaped/lshaped.py
@@ -1,6 +1,146 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
 import cplex
 from cplex.exceptions import CplexSolverError
-import numpy as np
+
+
+class Lshaped(object):
+
+    def __init__(self, *args, **kwargs):
+        self.master = cplex.Cplex(args[0])
+        self.master.set_results_stream(None)
+        self.master.solve()
+        self.xnames = self.master.variables.get_names()
+        self.x0 = self.master.solution.get_values()
+
+        self.names = None
+        self.Er = None
+        self.er = None
+
+        self.subproblems = args[1]
+        self.probs = args[2]
+        self.num_scen = args[3]
+        self.xvec = []
+        self.theta = None
+        self.teta = [0.0]
+        self.obj_value = [-cplex.infinity]
+        self.Stop_Feas = False
+
+
+    def _add_feasibility_cut(self):
+        for k in range(self.num_scen):
+            subproblem = cplex.Cplex(self.subproblems[k])
+            con_names = subproblem.linear_constraints.get_names()
+            # T matrix:
+            Tmp = []
+            for con in con_names:
+                Tmp.append([subproblem.linear_constraints.get_coefficients(con, xname) for xname in self.xnames])
+            T = np.array(Tmp)
+            h = np.array([subproblem.linear_constraints.get_rhs(con) for con in subproblem.linear_constraints.get_names()])
+
+            for yname in subproblem.variables.get_names(): 
+                subproblem.objective.set_linear( yname, 0.0)
+            ## ad v+ and v-:
+            for con in subproblem.linear_constraints.get_names():
+                subproblem.variables.add( obj = [1.0], lb = [0.0], ub = [cplex.infinity],
+                                          columns=[cplex.SparsePair(ind = [con], val = [1.0])] )
+            for con in subproblem.linear_constraints.get_names():
+                subproblem.variables.add( obj = [1.0], lb = [0.0], ub = [cplex.infinity],
+                                          columns=[cplex.SparsePair(ind = [con], val = [-1.0])] )
+
+            for (idx, xname) in enumerate(self.xnames):
+                subproblem.linear_constraints.add( lin_expr = [cplex.SparsePair(ind = [str(xname)], val = [1.0])], senses=["E"], rhs=[self.x0[idx]] )
+            subproblem.set_results_stream(None)
+            subproblem.solve()
+            duals = subproblem.solution.get_dual_values(con_names)
+
+            if subproblem.solution.get_objective_value() > 0.0:
+                self.Stop_Feas = False
+                Dr = np.dot(T.transpose(), duals)
+                dr = np.dot(h.transpose(), duals)
+                self.master.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = self.xnames, val = Dr)], senses=["G"], rhs=[dr])
+                print "{} >= {}".format(Dr, dr)
+                self.master.solve()
+                self.x0 = self.master.solution.get_values()
+                print self.x0
+
+
+    def _set_optimality_cut(self):
+        self.Er = [0.0] * len(self.xnames)
+        self.er = 0.0
+        for k in range(self.num_scen):
+            subproblem = cplex.Cplex(self.subproblems[k])
+            con_names = subproblem.linear_constraints.get_names()
+            # T matrix:
+            Tmp = []
+            for con in con_names:
+                Tmp.append([subproblem.linear_constraints.get_coefficients(con, xname) for xname in self.xnames])
+            T = np.array(Tmp)
+            h = np.array([subproblem.linear_constraints.get_rhs(con) for con in subproblem.linear_constraints.get_names()])
+
+            for i in range(len(self.xnames)):
+                subproblem.linear_constraints.add( lin_expr = [cplex.SparsePair(ind = [str(self.xnames[i])], val = [1.0])], senses=["E"], rhs=[self.x0[i]] )
+            subproblem.set_results_stream(None)
+            subproblem.solve()
+            duals = subproblem.solution.get_dual_values(con_names)
+
+            self.Er += self.probs[k]*np.dot(T.transpose(), duals)
+            self.er += self.probs[k]*np.dot(h.transpose(), duals)
+
+
+    def _check_termination(self):
+        if ( (self.er - sum(self.Er[:]*self.x0[:])) <= self.theta ):
+            print "[STOP]"
+            # print " * Optimal solution:\t x0 = {} \t theta = {}".format(x0, master.solution.get_values()[-1])
+            # print " \t\t\t Obj. value = {}".format(master.solution.get_objective_value())
+            self.teta[0] = self.master.solution.get_values()[-1]
+            self.obj_value[0] = self.master.solution.get_objective_value()
+            self.xvec += self.x0
+            return True
+        return False
+
+
+    def _add_optimality_cut(self):
+        self.Er = np.append( self.Er, 1.0 )
+        self.master.linear_constraints.add(lin_expr = [cplex.SparsePair(ind = self.names, val = self.Er)], senses=["G"], rhs=[self.er])
+        print "{} >= {}".format(self.Er, self.er)
+        self.master.solve()
+        self.x0 = self.master.solution.get_values()
+        del self.x0[-1]
+
+
+    def solve(self):
+        print ">> Feasibility cuts:"
+        while (self.Stop_Feas == False):
+            self.Stop_Feas = True
+            self._add_feasibility_cut()
+        # add theta:
+        self.master.variables.add( obj=[1.0], lb=[-cplex.infinity], ub=[cplex.infinity], names=['theta'])
+        self.names = self.master.variables.get_names()
+        print ">> Optimality cut:"
+        self._set_optimality_cut()
+        self._add_optimality_cut()
+        self.theta = self.x0[-1]
+
+        while True:
+            Stop_Feas = False
+            print ">> Feasibility cuts:"
+            while (self.Stop_Feas == False):
+                self.Stop_Feas = True
+                self._add_feasibility_cut()
+            print ">> Optimality cut:"
+            self._set_optimality_cut()
+            if (self._check_termination()):
+                break
+            self._add_optimality_cut()
+
+
+    def print_solution(self):
+        print " * Optimal solution:\t x0 = {} \t theta = {}".format(self.xvec, self.teta)
+        print " \t\t\t Obj. value = {}".format(self.obj_value)
+
 
 def lshaped(master_arg, subproblems, probs, num_scen, x, teta, obj_value):
 
--- lshaped/main.py
+++ lshaped/main.py
@@ -1,27 +1,28 @@
 #!/usr/bin/python
 
+import numpy as np
+
 import cplex
 from cplex.exceptions import CplexSolverError
-import numpy as np
 
 from lshaped import *
 
 num_scen = 0
 probs = []
 
-data = np.loadtxt('./scenario.dat',delimiter="\n")
+data = np.loadtxt('./farmer/scenario.dat',delimiter="\n")
 num_scen = int(data[0])
 for i in range(1,num_scen+1):
     probs.append(data[i])
 
 
-master = cplex.Cplex('./master.lp')
+master = cplex.Cplex('./farmer/master.lp')
 
 subproblems = []
-subproblems.append(cplex.Cplex('./subproblem1.lp'))
-subproblems.append(cplex.Cplex('./subproblem2.lp'))
-subproblems.append(cplex.Cplex('./subproblem3.lp'))
-subproblems.append(cplex.Cplex('./subproblem4.lp'))
+subproblems.append(cplex.Cplex('./farmer/subproblem1.lp'))
+subproblems.append(cplex.Cplex('./farmer/subproblem2.lp'))
+subproblems.append(cplex.Cplex('./farmer/subproblem3.lp'))
+# subproblems.append(cplex.Cplex('./subproblem4.lp'))
 if len(subproblems) != num_scen:
     print "PANIC"
 
@@ -33,3 +34,7 @@ lshaped(master, subproblems, probs, num_scen, x, teta, obj_value)
 print " * Optimal solution:\t x0 = {} \t theta = {}".format(x, teta)
 print " \t\t\t Obj. value = {}".format(obj_value)
 
+print "---------------- OOP ----------------"
+lshape = Lshaped(master, subproblems, probs, num_scen)
+lshape.solve()
+lshape.print_solution()
